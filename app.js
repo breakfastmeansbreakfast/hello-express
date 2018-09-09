9ef141a17636b8317a8544dad6f1970b0d6e3231
breakfastmeansbreakfast <77744c@gmail.com>
2018-09-09 18:15:16 +0100

    completed post request to file using fs module

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const helloWorld = require('./controllers/helloWorld');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const createShoppingList = require('./controllers/createShoppingList');
 
 app.get('/', helloWorld);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.post('/shoppingLists', createShoppingList);
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
